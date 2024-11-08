fc76c8cc5f4e9f74e677b26b4ae510461044fbe8
dheeraj0505 <dheerajshukla9895@gmail.com>
2024-11-08 23:17:34 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,20 +1,12 @@
 import os
-from flask import Flask, jsonify
-from pytrends.request import TrendReq
+from flask import Flask
 
 app = Flask(__name__)
 
-@app.route('/trending', methods=['GET'])
-def get_trending_topics():
-    # Initialize pytrends and set the region for trends
-    pytrends = TrendReq(hl='en-US', tz=360)
-    trending_searches_df = pytrends.trending_searches(pn='united_states')
-    trending_topics = trending_searches_df[0].tolist()
-    
-    return jsonify(trending_topics=trending_topics)
+@app.route('/')
+def home():
+    return "Hello, Render!"
 
 if __name__ == '__main__':
-    # Print statement for debugging
-    print("Starting the app and binding to port.")
-    port = int(os.environ.get("PORT", 5000))  # Use PORT environment variable
+    port = int(os.environ.get("PORT", 5000))
     app.run(host='0.0.0.0', port=port)
